3d46ddc4581c266a63bd31e3e5033bfa5959521f
shift14wx <carloshernandez.isaias12.2@gmail.com>
2022-03-04 17:29:25 -0600

    :sparkles: adding reload of images per item

--- src/screens/main/DetailEmployeeScreen.js
+++ src/screens/main/DetailEmployeeScreen.js
@@ -1,11 +1,13 @@
 import React, {useEffect} from "react";
-import {Card, Paragraph, Title, Avatar, Text} from "react-native-paper";
+import {Card, Paragraph, Title, Avatar, Text, ActivityIndicator} from "react-native-paper";
+import {useIsFocused} from "@react-navigation/native";
 
 export default function DetailEmployeeScreen(props) {
     const {route} = props;
     const employee = route.params;
-
     const LeftContent = props => <Avatar.Icon {...props} icon="account"/>
+    const isFocused = useIsFocused();
+    console.log(isFocused);
     return (
         <Card>
             <Card.Title title={`Employee #${employee.id}`} subtitle="Details" left={LeftContent}/>
@@ -15,7 +17,13 @@ export default function DetailEmployeeScreen(props) {
                     <Text>{`${employee.address["country"]}, ${employee.address["city"]}, ${employee.address["streetName"]}`}</Text>
                 </Paragraph>
             </Card.Content>
-            <Card.Cover source={{uri: employee.image}}/>
+            {
+                isFocused ?
+
+                    <Card.Cover source={{uri: isFocused ? employee.image+"?id="+employee.id : "https://crm.sprout.online/images/logo-sprout-v2.png?1" }}/>
+                    :
+                    <ActivityIndicator/>
+            }
             <Card.Actions>
 
             </Card.Actions>
